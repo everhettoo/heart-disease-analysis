ae3ddb1fa63ce71abc7d1f3b584b0b38801dfda7
everhettoo <everhettoo@gmail.com>
2024-12-17 13:56:28 +0800

    Added corr matrix and investigation in EDA

--- custom_libs/helper.py
+++ custom_libs/helper.py
@@ -1,25 +1,39 @@
-import pandas as pd
-# Function to calculate missing values by column
-def missing_values_table(df):
-    # Total missing values
-    mis_val = df.isnull().sum()
+import matplotlib.pyplot as plt;
+import seaborn as sns;
 
-    # Percentage of missing values
-    mis_val_percent = 100 * df.isnull().sum() / len(df)
+binary_color=['steelblue', 'darksalmon']
+single_color=['darksalmon']
 
-    # Make a table with the results
-    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+def draw_barchart(df, key, binary):
+    if binary:
+        df[key].value_counts().plot(kind="bar", color=binary_color);
+    else:
+        df[key].value_counts().plot(kind="bar", color=single_color);
 
-    # Rename the columns
-    mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Counts', 1 : 'Percentage'})
+def draw_histogram_density_curve(df, key):
+    sns.histplot(df[key], kde=True, color='darksalmon', edgecolor='grey', alpha=0.7)
 
-    # Sort the table by percentage of missing descending
-    mis_val_table_ren_columns = mis_val_table_ren_columns[mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
-        'Percentage', ascending=False).round(1)
+    plt.title('Histogram with Density Curve')
+    plt.xlabel(key)
+    plt.ylabel('Density')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
 
-    # Return the dataframe with missing information
-    return mis_val_table_ren_columns
+def draw_histogram(df, key):
+    min_value = int(df[key].min())
+    max_value = int(df[key].max())
+    bins = range(min_value, max_value + 2)
+
+
+    plt.hist(df[key], bins=bins, color='darksalmon', alpha=0.7, edgecolor='grey', rwidth=0.9, density=True)
+    plt.xticks(bins)
+    plt.title(f'Histogram of {key}')
+    plt.xlabel(key)
+    plt.ylabel('Frequency')
+    plt.grid(axis='y', color='gray', linestyle='--', linewidth=0.7, alpha=0.7)
+    plt.show()
 
 def value_count(df, key, value):
     # Total missing values
     return len(df[df[key] == value]) / len(df) * 100
+
